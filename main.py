451fc477caba62b8b3bffbb51a03c8036e0b493d
Lucas Lourenço <dev.lucaslourenco@gmail.com>
2023-09-30 15:31:36 -0300

    att

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import re
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 import os
-from docx2pdf import convert
+from docx2pdf import convert # pip install docx2pdf
 import copy
 import pandas as pd
 import docx
@@ -12,27 +13,32 @@ import locale
 class SimSalabim:
     locale.setlocale(locale.LC_MONETARY, '')
     
-    def __init__(self, documentos:list, base_dados, criarPDF:bool = False):
+    def __init__(self, documento:list, base_dados: pd.DataFrame, criarPDF:bool = False, diretorio_destino: Path = f'Documentos Criados - SimSalabim', delimitador:str = "==", fonte_letra:str = 'Times New Roman'):
         self.base_dados = base_dados
         
-        self.diretorio_para_salvar = 'Documentos Criados'
-        self.nome_documentos:list = documentos
+        self.diretorio_para_salvar = diretorio_destino
+        self.nome_documentos:list = documento
+        self.doc_base = docx.Document(documento)
+        self.fonte_letra = fonte_letra
         
         self.criar_pdf = criarPDF
         
         self.relacao_dataframe = self.dados_dataframe()
-        self.relacao_individual_funcionario:list[tuple[dict, str]] = list()
+        self.relacao_individual_funcionario:list[dict] = list()
+
+        self.delimitador = delimitador
+        self.regex_delimitados = list()
         
         self.analisar_delimitadores()
         
         self.relacao_informacoes()
-        self.separacao_criacao_documentos()
+        self.criar_documento()
         self.transformar_pdf()
         
         
-    @staticmethod
-    def adaptacao_salario(valor:float):
-        return locale.currency(valor, grouping=True)
+    # @staticmethod
+    # def adaptacao_salario(valor:float):
+    #     return locale.currency(valor, grouping=True) 
     
         
     def transformar_pdf(self):
@@ -44,20 +50,20 @@ class SimSalabim:
                 convert(arquivo_docx)
 
         
-    def dados_dataframe(self):
-        pl_df = pd.read_excel(self.base_dados)
-        pl_df = pl_df.sort_values(['NOME'])
-        pl_df = pl_df.dropna(how='any')
-        return pl_df
+    # def dados_dataframe(self):
+    #     pl_df = pd.read_excel(self.base_dados)
+    #     pl_df = pl_df.sort_values(['NOME'])
+    #     pl_df = pl_df.dropna(how='any')
+    #     return pl_df
         
     
     def relacao_informacoes(self) -> tuple[dict, str]:
         for index, row in self.relacao_dataframe.iterrows():
-            nome = row['NOME']
-            movimentacao = row['MOVIMENTACAO']
-            funcao = row['Cargo plano salario']
-            salario = row['Tabela Salarial']
-            ctps = row['CTPS']
+            # nome = row['NOME']
+            # movimentacao = row['MOVIMENTACAO']
+            # funcao = row['Cargo plano salario']
+            # salario = row['Tabela Salarial']
+            # ctps = row['CTPS']
             
             relacao:dict = {
                 '==NOME==':nome,
@@ -65,36 +71,32 @@ class SimSalabim:
                 '==FUNCAO==':funcao,
                 '==SALARIO==':self.adaptacao_salario(salario),
             }
-            
-            self.relacao_individual_funcionario.append((relacao, movimentacao.strip()))
+
+            self.relacao_individual_funcionario.append(relacao)
             
 
-    # def separacao_criacao_documentos(self):
-    #     for relacao, movimentacao in self.relacao_individual_funcionario:
-    #         print(relacao, movimentacao)
-            
-    #         match movimentacao:
-    #             case "PROMOÇÃO":
-    #                 self.criar_documento(doc_nome=self.nome_documentos[1], dados=relacao, nome_doc = 'PROMOÇÃO')
-    #             case "Mudança de função": 
-    #                 self.criar_documento(doc_nome=self.nome_documentos[0], dados=relacao, nome_doc='Mudança de função')
-                    
     def analisar_delimitadores(self):
-        ...        
+        regex = f'{self.delimitador}([^=]+){self.delimitador}'
+
+        for paragrafo in self.doc_base.paragraphs:
+            texto = paragrafo.text
+            
+            correspondencias = re.findall(regex, texto)
+            self.regex_delimitados.extend(correspondencias)            
         
                     
-    def criar_documento(self,doc_nome:str, dados:dict, nome_doc:str):
-        doc_base = docx.Document(doc_nome)
-        doc_base = copy.deepcopy(doc_base)   
+    def criar_documento(self, dados:dict, nome_doc:str):
+        doc_base_dc = copy.deepcopy(self.doc_base)   
 
-        for paragrafo in doc_base.paragraphs:
+        for paragrafo in doc_base_dc.paragraphs:
             for key, value in dados.items():
                 if key in paragrafo.text:
                     paragrafo.text = paragrafo.text.replace(key, str(value))
                     for run in paragrafo.runs:
-                        run.font.name = 'Times New Roman'
+                        run.font.name = self.fonte_letra
                         
-        for table in doc_base.tables:
+
+        for table in doc_base_dc.tables:
             for row in table.rows:
                 for cell in row.cells:
                     for key, value in dados.items():
@@ -104,13 +106,13 @@ class SimSalabim:
                             for paragraph in cell.paragraphs:
                                 for run in paragraph.runs:
                                     run.bold = True
-                                    run.font.name = 'Times New Roman'
+                                    run.font.name = self.fonte_letra
 
-        doc_base.save(Path(f'{self.diretorio_para_salvar}/{dados.get("==NOME==")} - {nome_doc}.docx'))            
+        doc_base_dc.save(Path(f'{self.diretorio_para_salvar}/{dados.get("==NOME==")} - {nome_doc}.docx'))            
         
         
         
 if __name__ =='__main__':
-    app = SimSalabim(documentos=[r'MOVIMENTAÇÃO DE PESSOAL - MUDANÇA DE FUNÇÃO.docx', r'MOVIMENTAÇÃO DE PESSOAL - PROMOÇÃO.docx'], criarPDF=True, base_dados=r'base.xlsx')
+    app = SimSalabim(documento=r'MOVIMENTAÇÃO DE PESSOAL - MUDANÇA DE FUNÇÃO.docx', criarPDF=False, base_dados=r'base.xlsx')
     
 ## CRIAR ALGORITMO PARA ANALISAR UM DOCX E VER, DEFININDO O DELIMITADOR ANTES "==", QUAIS SAO. DEPOIS, CRIAR UM DICT CONTENDO O DELIMITADOR E RESPECTIVAMENTE OS DADOS QUE POSSUEM O MESMO NOME NA COLUNA DA BASE.XLSX
